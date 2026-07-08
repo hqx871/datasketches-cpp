b08342fd85f66b93a067e781af2a33880bef7f21
huangqixiang <huangqixiang.871@bytedance.com>
2026-07-09 01:26:16 +0800

    merge set

--- hll/include/HllUnion-internal.hpp
+++ hll/include/HllUnion-internal.hpp
@@ -122,7 +122,7 @@ void hll_union_alloc<A>::update(const void* data, size_t length_bytes) {
 template<typename A>
 void hll_union_alloc<A>::coupon_update(uint32_t coupon) {
   if (coupon == hll_constants::EMPTY) { return; }
-  HllSketchImpl<A>* result = gadget_.sketch_impl->coupon_update(coupon);
+  HllSketchImpl<A>* result = gadget_.sketch_impl->couponUpdate(coupon);
   if (result != gadget_.sketch_impl) {
     if (gadget_.sketch_impl != nullptr) { gadget_.sketch_impl->get_deleter()(gadget_.sketch_impl); }
     gadget_.sketch_impl = result;
